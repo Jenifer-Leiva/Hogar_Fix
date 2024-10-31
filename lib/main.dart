04dd0e5550e500184ef781f684e422ee44f32880
jenifer leiva <lizetheleiva.3@gmail.com>
2024-10-31 11:54:34 -0500

    otros cambios

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,10 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hogar Fix'),
         ),
       ),
     );
   }
 }
+ 
